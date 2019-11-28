51eca3baba6f033f0ee8a070a95a8c481c9e406e
HappyDay <happyday.mjohnson@gmail.com>
2019-11-28 14:37:41 +0000

    added mongo_to_pandas script

--- data_results/json_to_pickle.py
+++ data_results/json_to_pickle.py
@@ -1,7 +1,11 @@
 import pandas as pd
+import sys
+# Get the name of the json file
+filename = sys.argv[1]
 #
 # At first I got ValueError: Trailing data. I added lines=True.
-df = pd.read_json('aggregated.json', lines=True)
+df = pd.read_json(filename+'.json', lines=True)
+# TBD: Columns to drop... basically all columns that arent Pr, Pa, I...Make this more robust...
 df.drop(['_id', 'microwave'], axis=1, inplace=True)
 df = df.set_index('timestamp')
-df_pickled = df.to_pickle('aggregated.pkl')
+df_pickled = df.to_pickle(filename+'.pkl')
