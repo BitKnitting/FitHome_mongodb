e765c58b984a4dd8a954319fd62264faeabaeb51
HappyDay <happyday.mjohnson@gmail.com>
2019-12-01 16:23:41 +0000

    added data

--- data_results/json_to_pickle.py
+++ data_results/json_to_pickle.py
@@ -6,7 +6,7 @@ filename = sys.argv[1]
 # At first I got ValueError: Trailing data. I added lines=True.
 df = pd.read_json(filename+'.json', lines=True)
 # TBD: Columns to drop... basically all columns that arent Pr, Pa, I...Make this more robust...
-df.drop(['_id', 'microwave'], axis=1, inplace=True)
+df.drop(['_id'], axis=1, inplace=True)
 df = df.set_index('timestamp')
 # Set the time to PST
 df.index = df.index.tz_localize('UTC').tz_convert('US/Pacific')
